f308b49d81884997e8b4ca79743b761b5f851a37
Yuki Ito <yuki@yukis-air.wlan.rose-hulman.edu>
2016-03-15 19:55:27 -0400

    add connection.py

---
+++ connection.py
@@ -0,0 +1,7 @@
+import requests
+
+def get_state():
+
+
+def request():
+    return requests.get('https://enova-no-limit-code-em.herokuapp.com/sandbox/players/1ad1982-c2c0-491f-87c1-5c330bdc92a7').json()
